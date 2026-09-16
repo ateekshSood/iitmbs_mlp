1e7c5405d0a4965ce27f5f5a9bd414a66a1f3f52
Ateeksh <ateesood@gmail.com>
2026-09-16 06:14:06 +0000

    week 1 done

--- w1/l1.py
+++ w1/l1.py
@@ -1,3 +1,31 @@
 # %%
-print("hello")
+import pandas as pd
+import numpy as np
 # %%
+
+#check version 
+
+print(pd.__version__)
+# %%
+
+from sklearn.datasets import load_diabetes 
+
+diabetes = load_diabetes(as_frame = True)
+ # without this it will  load bunch
+print(type(diabetes['data']))
+# %%
+
+df = diabetes['data']
+df.head(4)
+# %%
+#creating series 
+
+
+cities = pd.Series(['Mumabi' , 'Banglore' , 'Chennai' ,"yeh"])
+population = pd.Series([10000 , 100000 , 10000])
+
+df = pd.DataFrame( {"City" : cities , "Populaiton" : population} )
+df
+df.fillna(df['Populaiton'].mean())
+# %%
+
